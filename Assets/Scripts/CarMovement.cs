968373f483af8179ca17f0063b69d89494a6a048
Aisyah Tria <aisyahnyawn@gmail.com>
2019-10-19 14:31:40 +0700

    tugas driving

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float turnRate = 5f;
+    [SerializeField] float brakeRate = 10f;
+    int gear = 0;
+    Rigidbody rb = null;
+    float gas = 0f;
+    float steer = 0f;
+
+    int turnDir = 0;
+    Vector3 vel = new Vector3();
+    WheelCollider fLeft, fRight, rLeft, rRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        fLeft = transform.GetChild(0).GetComponent<WheelCollider>();
+        fRight = transform.GetChild(1).GetComponent<WheelCollider>();
+        rLeft = transform.GetChild(2).GetComponent<WheelCollider>();
+        rRight = transform.GetChild(3).GetComponent<WheelCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gas = Input.GetAxis("Gaz");
+        steer = Input.GetAxis("Steer");
+
+        rLeft.motorTorque = gas * moveSpeed * gear;
+        rRight.motorTorque = gas * moveSpeed * gear;
+
+        fLeft.steerAngle = steer * turnRate;
+        fRight.steerAngle = steer * turnRate;
+
+        if(gas == 0)
+        {
+            Debug.Log("brake");
+            rLeft.brakeTorque = brakeRate;
+            rRight.brakeTorque = brakeRate;
+        }
+        else
+        {
+            rLeft.brakeTorque = 0;
+            rRight.brakeTorque = 0;
+        }
+
+        //if (gas >= 0.1f)
+        //{
+        //    vel = transform.forward * moveSpeed * gear * gas;
+        //    //vel.y = rb.velocity.y;
+        //    //rb.velocity = vel;
+        //    rb.AddForce(vel, ForceMode.Acceleration);
+        //}
+        //else
+        //{
+        //    vel *= 0;
+        //    //vel.y = rb.velocity.y;
+        //    //rb.velocity = vel;
+        //    rb.AddForce(vel, ForceMode.Acceleration);
+        //}
+
+        //if(rb.velocity != Vector3.zero)
+        //    Turn();
+
+        if(Input.GetButtonDown("Shift Up"))
+        {
+            ShiftUp();
+        }
+        else if(Input.GetButtonDown("Shift Down"))
+        {
+            ShiftDown();
+        }
+    }
+
+    public void ShiftUp()
+    {
+        gear += 1;
+        if(gear > 0)
+        {
+            turnDir = 1;
+        }
+    }
+
+    public void ShiftDown()
+    {
+        gear -= 1;
+        if(gear < 0)
+        {
+            turnDir = -1;
+        }
+    }
+
+    public void Turn()
+    {
+        transform.Rotate(Vector3.up, turnRate * steer * gas * turnDir * Time.deltaTime);
+        rb.AddRelativeForce(-Vector3.right * rb.velocity.magnitude * steer);
+    }
+}
---
+++ Assets/Scripts/TesControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Shift Down"))
+        {
+            Debug.Log("Shift Down");
+        }
+        if (Input.GetButtonDown("Shift Up"))
+        {
+            Debug.Log("Shift Up");
+        }
+    }
+}
